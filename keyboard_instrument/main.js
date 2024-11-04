a1fd44fbcbe18bf5c7da26030f439c7f3fb31071
dudugan <dvzar27@gmail.com>
2024-11-04 13:29:54 -0500

    populate keyboard thing

--- keyboard_instrument/main.js
+++ keyboard_instrument/main.js
@@ -1,4 +1,15 @@
 document.body.onkeydown = function(keypress_event){
   var key = keypress_event.key;
-  document.getElementById("key_display").innerHTML = key;
+  if (key == "r"){
+    document.body.style.backgroundColor = "pink"; 
+  } else if (key == "g"){
+    document.body.style.backgroundColor = "orange"; 
+  }
+  var new_element = document.createElement("h1"); 
+  new_element.innerHTML = key; 
+  new_element.classList.add("example_class"); 
+  new_element.style.fontSize = "100px"; 
+  document.body.appendChild(new_element); 
 };
+
+// change image source
