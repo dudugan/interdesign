0f00fee746189407bd6bf8210edc41fcd2ac281d
dudugan <dvzar27@gmail.com>
2024-11-23 13:06:48 -0500

    maybe better

--- synesthesiac/popup.js
+++ synesthesiac/popup.js
@@ -26,6 +26,8 @@ document.addEventListener("DOMContentLoaded", () => {
             chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
                 chrome.tabs.sendMessage(tabs[0].id, {reset: true}); 
             });
+        } else {
+            applyColors(data); 
         }
     });
 
