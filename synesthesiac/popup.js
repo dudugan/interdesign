7b9c96d61e344235c3ed03736597e452a4fcd0ea
dudugan <dvzar27@gmail.com>
2024-11-23 09:20:35 -0500

    wrapped js in wait for DOM to load wrapper

--- synesthesiac/popup.js
+++ synesthesiac/popup.js
@@ -1,25 +1,28 @@
-document.getElementById("apply-colors").addEventListener("click", () => {
-    const textColor = document.getElementById("text-color").value; 
-    const bgColor = document.getElementById("bg-color").value; 
-
-    console.log("Colors selected:", { textColor, bgColor });
-
-    chrome.tabs.query({active: true, currentWindow: true }, (tabs) => {
-        if (tabs.length > 0){
-            console.log("Sending message to content.js..."); 
-
-            // make sure script is injected even if page was loaded before extension
-            chrome.scripting.executeScript(
-                {
-                    target: {tabId: tabs[0].id},
-                    files: ["content.js"],
-                }, 
-                () => {
-                    chrome.tabs.sendMessage(tabs[0].id, {textColor, bgColor });
-                }
-            )  
-        } else {
-            console.error("No active tabs found :("); 
-        }
+// check that DOM loaded before start to make changes
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("apply-colors").addEventListener("click", () => {
+        const textColor = document.getElementById("text-color").value; 
+        const bgColor = document.getElementById("bg-color").value; 
+    
+        console.log("Colors selected:", { textColor, bgColor });
+    
+        chrome.tabs.query({active: true, currentWindow: true }, (tabs) => {
+            if (tabs.length > 0){
+                console.log("Sending message to content.js..."); 
+    
+                // make sure script is injected even if page was loaded before extension
+                chrome.scripting.executeScript(
+                    {
+                        target: {tabId: tabs[0].id},
+                        files: ["content.js"],
+                    }, 
+                    () => {
+                        chrome.tabs.sendMessage(tabs[0].id, {textColor, bgColor });
+                    }
+                )  
+            } else {
+                console.error("No active tabs found :("); 
+            }
+        }); 
     }); 
-}); 
+})
