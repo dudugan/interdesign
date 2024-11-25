4935f4fc27a61ce7d85384b3ad800329872f3247
dudugan <dvzar27@gmail.com>
2024-11-25 10:29:52 -0500

    wrote a ton of important backup fxns

---
+++ project2/file_comprehension.js
@@ -0,0 +1,42 @@
+// ** CONSTRUCTS CHORD OBJECTS **
+// to create a new chord: "const chord1 = new Chord("C", "maj");"
+function Chord(root, type){
+    // properties
+    this.root = root;
+    this.type = type; // maj/min
+    this.fifth = move(root, 7); // 7 bc we're moving in half-steps
+    if (this.type == 'maj'){
+        this.third = move(root, 4); 
+        this.seventh = move(root, 11); 
+    } else if (this.type == 'min'){
+        this.third = move(root, 3); 
+        this.seventh = move(root, 10); 
+    }
+
+    // methods (use functions in play_music)
+    this.play = synthate;
+    this.arpeggiate = arpeggiate;
+}
+
+// ** READS DNA FILE INTO LIST OF CHORD OBJECTS **
+async function processFile(){
+    const fileInput = document.getElementById('upload'); 
+
+    // check for file existence
+    if (fileInput.files.length === 0){
+        alert('please select a file');
+        return; 
+    }
+
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    // runs whenever the reader is done reading the file content
+    reader.onload = async function(event){
+        fileContent = event.target.result;
+        initializeAudio(); 
+    }
+
+    // reads the file content
+    reader.readAsText(file); 
+}
--- project2/play_music.js
+++ project2/play_music.js
@@ -1,3 +1,35 @@
 // include Tone.js
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tone/15.1.3/Tone.js"></script>
 
+// for moving from one note to another
+function move(start, n){
+    const notes = ["Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G"];
+    const startIndex = notes.indexOf(start);
+
+    // check start index
+    if (startIndex === -1){
+        console.error("bad start note for move");
+        return; 
+    }
+
+    // get new index
+    const newIndex = (startIndex + n) % notes.length;
+
+    // if negative index (probably bc went backwards), add length of notes array
+    const wrappedIndex = newIndex < 0 ? notes.length + newIndex : newIndex; 
+
+    // return note string
+    return notes[wrappedIndex];
+}
+
+function initializeAudio(){
+
+}
+
+function synthate(chord){
+
+}
+
+function arpeggiate(chord){
+
+}
